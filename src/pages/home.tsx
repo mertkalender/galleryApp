3c9356dcc7be0ddf596ca563adcd05a6f337181c
Mert Kalender <kalender.mert00@gmail.com>
2023-03-19 17:34:12 +0300

    Initialize routing

---
+++ src/pages/home.tsx
@@ -0,0 +1,19 @@
+import {
+  SafeAreaView,
+  ScrollView,
+  View,
+} from 'react-native';
+
+
+export function HomePage(): JSX.Element {
+
+  
+    return (
+      <SafeAreaView >
+        <ScrollView contentInsetAdjustmentBehavior="automatic">
+          <View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    );
+  }
---
+++ src/pages/imageDetails.tsx
@@ -0,0 +1,19 @@
+import {
+  SafeAreaView,
+  ScrollView,
+  View,
+} from 'react-native';
+
+
+
+export function ImageDetailsPage(): JSX.Element {
+
+    return (
+      <SafeAreaView >
+        <ScrollView contentInsetAdjustmentBehavior="automatic">
+          <View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    );
+  }
